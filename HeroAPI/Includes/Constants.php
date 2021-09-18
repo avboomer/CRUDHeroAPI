9fd0977a452fdca2ae81e8a8b89c9fad2985a1e7
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-09-18 16:39:08 -0300

    Commit inicial

---
+++ HeroAPI/Includes/Constants.php
@@ -0,0 +1,5 @@
+<?php 
+	define('DB_HOST', 'localhost'); //IP máquina
+	define('DB_USER', 'etecia');
+	define('DB_PASS', '123456');
+	define('DB_NAME', 'android');
--- index.php
+++ index.php
@@ -1,88 +1,72 @@
 <?php 
+    //include_once 'PHPActions/ConexaoBD.php';
+	include_once 'Includes/Header.php';
+	//include_once 'includes/mensagem.php';
+?>   
+    <h1>Sistema de Gerenciamento de Heróis (SGH)</h1>
+    <h2>Criar herói</h2>
+    <form action="HeroAPI/V1/API.php?apicall=createhero" method="POST">
+        <div>
+            <label for="nome">Nome</label>
+            <input type="text" name="name" id="nome">
+        </div>
+        <div>
+            <label for="nome_verdadeiro">Nome Verdadeiro</label>
+            <input type="text" name="realname" id="nome_verdadeiro">
+        </div>
+        <div>
+            <label for="classificacao">Classificação</label>
+            <input type="text" name="rating" id="classificacao">
+        </div>
+        <div>
+            <label for="afiliacao">Afiliação</label>
+            <input type="text" name="teamaffiliation" id="afiliacao">
+        </div>
+        
+        <button type="submit" name="m_btn_criar">Criar herói</button>
+    </form>
 
-	include_once 'php_action/conexao_bd.php';
+    <h2>Pesquisar heróis</h2>
+    <form action="HeroAPI/V1/API.php?apicall=getheroes" method="POST">
+        <button type="submit" name="m_btn_pesquisar">Pesquisar heróis</button>
+    </form>
 
-	//cabeçalho
-	include_once 'includes/header.php';
+    <h2>Alterar herói</h2>
+    <form action="HeroAPI/V1/API.php?apicall=updatehero" method="POST">
+        <div>
+            <label for="id">ID</label>
+            <input type="text" name="id" id="id">
+        </div>
+        <div>
+            <label for="nome">Nome</label>
+            <input type="text" name="name" id="nome">
+        </div>
+        <div>
+            <label for="nome_verdadeiro">Nome Verdadeiro</label>
+            <input type="text" name="realname" id="nome_verdadeiro">
+        </div>
+        <div>
+            <label for="classificacao">Classificação</label>
+            <input type="text" name="rating" id="classificacao">
+        </div>
+        <div>
+            <label for="afiliacao">Afiliação</label>
+            <input type="text" name="teamaffiliation" id="afiliacao">
+        </div>
 
-	include_once 'includes/mensagem.php';
-?>
+        <button type="submit" name="m_btn_criar">Alterar herói</button>
+    </form>
 
-	<div class="row">
-		<div class="col s12 m6 push-m3">
-			<h3 class="light">Clientes</h3>
-			<table class="striped">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Sobrenome</th>
-					<th>E-mail</th>
-					<th>Idade</th>
-				</tr>				
-			</thead>
-			
-			<tbody>
-				<?php 
+    <h2>Excluir herói</h2>
+    <form action="HeroAPI/V1/API.php" method="GET">
+        <div>
+            <label for="identificacao">ID</label>
+            <input type="hidden" name="apicall" value="deletehero">
+            <input type="text" name="id" id="identificacao">
+        </div>
 
-					$sql = "SELECT * FROM tbClientes";
-
-					$resultado = mysqli_query($connection, $sql);
-
-						
-					while($dados = mysqli_fetch_array($resultado)){
-
-					?>				
-			
-						<tr>
-							<td><?php echo $dados['nomeCli']; ?></td>
-							<td><?php echo $dados['sobreNomeCli']; ?></td>
-							<td><?php echo $dados['emailCli']; ?></td>
-							<td><?php echo $dados['idadeCli']; ?></td>
-
-							<td><a href="alterar.php?id=<?php echo $dados['codCli']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
-
-							<td><a href="#modal<?php echo $dados['codCli']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
-
-							<!-- Modal Structure in Materializecss -->
-							  <div id="modal<?php echo $dados['codCli']; ?>" class="modal">
-							    <div class="modal-content">
-							      <h4>Aviso.</h4>
-							      <p>Deseja excluir o cliente?</p>
-							    </div>
-							    <div class="modal-footer">
-							      
-
-							      <form action="php_action/excluir_cliente.php" method="POST">
-							      	<input type="hidden" name="codCli" value="<?php echo $dados['codCli']; ?>">
-
-							      	<button type="submit" name="btn-excluir" class="btn red">Excluir</button>
-
-							      	<a href="#!" class="modal-close waves-effect waves-green btn">Cancelar</a>
-
-							      </form>
-							    </div>
-							  </div>
-
-						</tr>
-						
-					<?php }
-
-					?>
-					
-			</tbody>
-
-			</table>
-			<br>
-			<a href="cadastrar.php" class="btn">Adicionar Cliente</a>
-		</div>		
-	</div>
-
-
+        <button type="submit">Excluir herói</button>
+    </form>
 <?php 
-	
-	//rodapé
-
 	include_once 'includes/footer.php';
-
-
- ?>
+?>
