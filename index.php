ed400b179eda59b4751e0a7d8d0d93ceeefd2a81
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-09-19 18:53:55 -0300

    Footer e Header includes e ajuste no form

--- index.php
+++ index.php
@@ -3,70 +3,112 @@
 	include_once 'Includes/Header.php';
 	//include_once 'includes/mensagem.php';
 ?>   
-    <h1>Sistema de Gerenciamento de Heróis (SGH)</h1>
-    <h2>Criar herói</h2>
-    <form action="HeroAPI/V1/API.php?apicall=createhero" method="POST">
-        <div>
-            <label for="nome">Nome</label>
-            <input type="text" name="name" id="nome">
+    <div class="container">
+        <h3>Sistema de Gerenciamento de Heróis (SGH)</h1>
+        <div class="row">
+            <form action="HeroAPI/V1/API.php?apicall=getheroes" method="POST">
+                <button type="submit" name="m_btn_pesquisar" class="btn waves-effect waves-light col s2 offset-s5">Listar heróis</button>
+            </form>
         </div>
-        <div>
-            <label for="nome_verdadeiro">Nome Verdadeiro</label>
-            <input type="text" name="realname" id="nome_verdadeiro">
-        </div>
-        <div>
-            <label for="classificacao">Classificação</label>
-            <input type="text" name="rating" id="classificacao">
-        </div>
-        <div>
-            <label for="afiliacao">Afiliação</label>
-            <input type="text" name="teamaffiliation" id="afiliacao">
-        </div>
-        
-        <button type="submit" name="m_btn_criar">Criar herói</button>
-    </form>
-
-    <h2>Pesquisar heróis</h2>
-    <form action="HeroAPI/V1/API.php?apicall=getheroes" method="POST">
-        <button type="submit" name="m_btn_pesquisar">Pesquisar heróis</button>
-    </form>
-
-    <h2>Alterar herói</h2>
-    <form action="HeroAPI/V1/API.php?apicall=updatehero" method="POST">
-        <div>
-            <label for="id">ID</label>
-            <input type="text" name="id" id="id">
-        </div>
-        <div>
-            <label for="nome">Nome</label>
-            <input type="text" name="name" id="nome">
-        </div>
-        <div>
-            <label for="nome_verdadeiro">Nome Verdadeiro</label>
-            <input type="text" name="realname" id="nome_verdadeiro">
-        </div>
-        <div>
-            <label for="classificacao">Classificação</label>
-            <input type="text" name="rating" id="classificacao">
-        </div>
-        <div>
-            <label for="afiliacao">Afiliação</label>
-            <input type="text" name="teamaffiliation" id="afiliacao">
+        <div class="divider"></div>
+        <div class="row">
+            <h4>Criar herói</h4>
+            <form action="HeroAPI/V1/API.php?apicall=createhero" method="POST">
+                <div class="col s3">
+                    <div class="input-field">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="name" id="nome" class="validate" required>
+                    </div>
+                </div>
+                <div class="col s3">
+                    <div class="input-field">
+                        <label for="nome_verdadeiro">Nome Verdadeiro</label>
+                        <input type="text" name="realname" id="nome_verdadeiro" class="validate" required>
+                    </div>
+                </div>
+                <div class="col s3">
+                    <div class="input-field">
+                        <label for="classificacao">Classificação</label>
+                        <input type="text" name="rating" id="classificacao" class="validate" required>
+                    </div>
+                </div>
+                <div class="col s3">
+                    <div class="input-field">
+                        <label for="afiliacao">Afiliação</label>
+                        <input type="text" name="teamaffiliation" id="afiliacao" class="validate" required>
+                    </div>
+                </div>
+                
+                <div class="">
+                    <button type="submit" name="m_btn_criar" class="btn waves-effect waves-light col s2 offset-s5">Criar herói</button>
+                </div>
+            </form>
         </div>
 
-        <button type="submit" name="m_btn_alterar">Alterar herói</button>
-    </form>
+        <div class="divider"></div>
 
-    <h2>Excluir herói</h2>
-    <form action="HeroAPI/V1/API.php" method="GET">
-        <div>
-            <label for="identificacao">ID</label>
-            <input type="hidden" name="apicall" value="deletehero">
-            <input type="text" name="id" id="identificacao">
+        <div class="row">
+            <h4>Alterar herói</h4>
+            <form action="HeroAPI/V1/API.php?apicall=updatehero" method="POST">
+                <div class="col s2">
+                    <div class="input-field">
+                        <label for="id">ID</label>
+                        <input type="text" name="id" id="id" required>
+                    </div>
+                </div>
+                <div class="col s2">
+                    <div class="input-field">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="name" id="nome" required>
+                    </div>
+                </div>
+                <div class="col s2">
+                    <div class="input-field">
+                        <label for="nome_verdadeiro">Nome Verdadeiro</label>
+                        <input type="text" name="realname" id="nome_verdadeiro" required>
+                    </div>
+                </div>
+                <div class="col s2">
+                    <div class="input-field">
+                        <label for="classificacao">Classificação</label>
+                        <input type="text" name="rating" id="classificacao" required>
+                    </div>
+                </div>
+                <div class="col s3">
+                    <div class="input-field">
+                        <label for="afiliacao">Afiliação</label>
+                        <input type="text" name="teamaffiliation" id="afiliacao" required>
+                    </div>
+                </div>
+
+                <div class="">
+                    <button type="submit" name="m_btn_alterar" class="btn waves-effect waves-light col s2 offset-s5">Alterar herói</button>
+                </div>
+            </form>
         </div>
 
-        <button type="submit">Excluir herói</button>
-    </form>
+        <div class="divider"></div>
+
+        <div class="row">
+            <h4>Excluir herói</h4>
+            <form action="HeroAPI/V1/API.php" method="GET">
+                <div class="col s2 offset-s4">
+                    <div class="input-field">
+                        <label for="identificacao">ID</label>
+                        <input type="hidden" name="apicall" value="deletehero">
+                        <input type="text" name="id" id="identificacao" required>
+                    </div>
+                </div>
+
+                <div class="">
+                    <button type="submit" class="btn waves-effect waves-light col s2">Excluir herói</button>
+                </div>
+            </form>
+
+            
+        </div>
+    </div>
+    
 <?php 
 	include_once 'includes/footer.php';
 ?>
